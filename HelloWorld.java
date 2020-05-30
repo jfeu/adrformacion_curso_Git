94eeaeac8f733c09cfb6423676ec566b73bdb963
Juan Feu <jfeu@cisco.com>
2020-05-31 00:14:40 +0200

    cambio

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
 
-    System.out.println("Hello world! Hola mundo háblame de ti y de tu gente!");
+    System.out.println("Hola mundo");
 
   }
 }
