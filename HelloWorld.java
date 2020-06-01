6dd89152c58ed51259862d5bc794d0529ceb6dfc
Juan Feu <jfeu@cisco.com>
2020-06-01 22:46:25 +0200

    mi cambio

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 
 
-    System.out.println("Hola mundo, este es mi test corregido");
+    System.out.println("Hola mundo");
 
 
   }
